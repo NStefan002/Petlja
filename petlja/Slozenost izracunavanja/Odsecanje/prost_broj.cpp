7e7d3b485d5307ede5cf154823078e55e9869b12
NStefan002 <n.stefan002@gmail.com>
2022-11-04 01:32:48 +0100

    Ima i fora za 6k +- 1

---
+++ petlja/Slozenost izracunavanja/Odsecanje/prost_broj.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool jesteProst(unsigned x);
+
+int main()
+{
+    unsigned n;
+    cin >> n;
+
+    cout << (jesteProst(n) ? "DA" : "NE") << endl; 
+
+    return 0;
+}
+
+bool jesteProst(unsigned x)
+{
+    if (x % 2 == 0 || x == 1)
+    {
+        return false;
+    }
+    
+    for (unsigned i = 3; i <= (unsigned)sqrt(x); i++)
+    {
+        if (x % i == 0)
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
