e450b811df81bbd22360e08135830b573a2233bb
NStefan002 <n.stefan002@gmail.com>
2022-11-07 01:04:27 +0100

    Logicno je da covek koji radi za salterom vidi prvu osobu koja je u redu...

---
+++ petlja/Slozenost izracunavanja/Inkrementalnost/Inkrementalnost minimuma i maksimuma/red_osoba.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    int brOsoba = 1;    // prvu osobu sigurno vidi 
+    int visina;
+    cin >> visina;
+    int maxVisina = visina;   // maxVisina inicijalizujemo na visinu prve osobe u redu
+
+    for (int i = 1; i < n; i++)
+    {
+        cin >> visina;
+        if (visina > maxVisina)
+        {
+            brOsoba++;
+            maxVisina = visina;
+        }
+    }
+    
+    cout << brOsoba << endl;
+
+    return 0;
+}
