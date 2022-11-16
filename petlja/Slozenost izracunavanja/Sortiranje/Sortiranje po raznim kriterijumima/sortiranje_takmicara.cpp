b57b6e89ff1a288b478947f1bdca18ab63a37081
NStefan002 <n.stefan002@gmail.com>
2022-11-16 17:34:56 +0100

    ne znam zasto ne radi sa string umesto statickog niza karaktera

---
+++ petlja/Slozenost izracunavanja/Sortiranje/Sortiranje po raznim kriterijumima/sortiranje_takmicara.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+
+#define MAX_IME 20
+
+
+using namespace std;
+
+
+struct Takmicar
+{
+    char ime[MAX_IME + 1];
+    int brPoena;
+};
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+
+    vector<Takmicar> takmicari(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> takmicari[i].ime >> takmicari[i].brPoena;
+    }
+    
+    sort(begin(takmicari), end(takmicari), 
+    [] (const Takmicar& t1, const Takmicar& t2)
+    {
+        if (t1.brPoena > t2.brPoena)
+        {
+            return true;
+        }
+        else if (t1.brPoena < t2.brPoena)
+        {
+            return false;
+        }
+        else if (strcmp(t1.ime, t2.ime) <= 0) // t1.ime <= t2.ime
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    });
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << takmicari[i].ime << ' ' << takmicari[i].brPoena << '\n';
+    }
+
+    return 0;
+}
