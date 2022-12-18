0c3489eecc58818edd35108f7502638172a4ecb2
NStefan002 <n.stefan002@gmail.com>
2022-12-18 16:57:27 +0100

    deque ima metode za dodavanje/skidanje elementa i sa pocetka i sa kraja reda

---
+++ petlja/Stek i red - osnove/Red/sortiranje-svi_ispred_manji_ili_svi_ispred_veci.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <deque>
+
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    deque<int> sortiranNiz;
+
+    for (int i = 0; i < n; i++)
+    {
+        int br;
+        cin >> br;
+
+        if (!sortiranNiz.empty() and br < sortiranNiz.back())
+        {
+            sortiranNiz.push_front(br);
+        }
+        else
+        {
+            sortiranNiz.push_back(br);
+        }
+    }
+    
+    for (auto it : sortiranNiz)
+    {
+        cout << it << ' ';
+    }
+    cout << endl;
+
+    return 0;
+}
